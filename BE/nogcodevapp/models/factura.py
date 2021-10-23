410865490f9f2b2c31765e9db6ea101f9b3efecd
Joselo <impacevanend@gmail.com>
2021-10-23 12:04:49 -0500

    Se acgrega estructura del proyecto

---
+++ BE/nogcodevapp/models/factura.py
@@ -0,0 +1,12 @@
+from django.db import models
+from .usuario import User
+class Factura(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, related_name='factura', on_delete=models.CASCADE)
+    fecha = models.DateTimeField()
+    
+    
+    
+    def __str__(self):
+        
+        return f'Factura {self.id}: {self.user} {self.fecha}'
